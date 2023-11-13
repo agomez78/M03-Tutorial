303f32e5b90bf85803e1d6d717e5e004e4136c20
agomez78 <149906558+agomez78@users.noreply.github.com>
2023-11-12 19:03:09 -0600

    Update Sort Array.py

--- Sort Array.py
+++ Sort Array.py
@@ -1,18 +1,4 @@
-def func(arr, low, high, k):
-    if low<=high:
-        mid = (low+high)//2
-        if arr[mid] == k:
-            return mid
-        elif arr[mid] < k:
-            return func(arr, mid+1, high, k)
-        else:
-            return func(arr, low, mid-1, k)
-    else:
-        return -1
-
-class Solution:    
-    def binarysearch(self, arr, n, k):
-
-        pos = func(arr, 0, n-1, k)
-        
-        return pos
+class Solution:
+    def sort012(self,arr,n):
+        array = arr
+        array.sort()
